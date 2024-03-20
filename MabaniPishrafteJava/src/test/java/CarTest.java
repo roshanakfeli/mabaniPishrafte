6243cdcf7e26a9bd090abafe607954159f4567bf
gitFeli <roshanakfeli1@gmail.com>
2024-03-20 13:37:52 +0330

    class Car created

---
+++ MabaniPishrafteJava/src/test/java/CarTest.java
@@ -0,0 +1,30 @@
+public class CarTest {
+
+    class Car {
+        // dade || data
+        boolean isOn;
+        boolean isOpen;
+
+        // method
+        void roshanAst(){
+            System.out.println("Mashin roshan ast.");
+            isOn = true;
+        }
+
+        void khamoshAst(){
+            System.out.println("Mashin khamosh ast.");
+            isOn = false;
+        }
+
+        void darBazAst(){
+            System.out.println("dar baz ast.");
+            isOpen = true;
+        }
+
+        void darBasteAst(){
+            System.out.println("dar baste ast.");
+            isOpen = false;
+        }
+    }
+
+}
